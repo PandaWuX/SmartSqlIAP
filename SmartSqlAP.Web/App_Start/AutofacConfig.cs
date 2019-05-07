dd509453c3dc36239a98fa23f5a67ba9bf4fce2d
吴吉顺 <wujishun@nxin.com>
2019-05-07 18:06:41 +0800

    first commit

---
+++ SmartSqlAP.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmartSqlAP.Web.App_Start
+{
+    public class AutofacConfig
+    {
+
+
+
+    }
+}
---
+++ SmartSqlAP.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmartSqlAP.Web.MvcApplication" Language="C#" %>
